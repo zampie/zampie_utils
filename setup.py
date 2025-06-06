47c598fc6c0c6d0db9b52703ac674bc445149943
zampie <zampielzp@gmail.com>
2025-06-07 05:02:10 +0800

    发布准备

--- setup.py
+++ setup.py
@@ -16,10 +16,30 @@ setup(
         "chardet",
         "colorlog",
     ],
-    author="liuzepeng01@baidu.com",
-    author_email="liuzepeng01@baidu.com",
+    author="zampie",
+    author_email="zampielzp@gmail.com",
     description="A collection of utility functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.6",
-) 
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="utils, tools, utilities",
+    url="https://github.com/zampie/zampie_utils",
+    project_urls={
+        "Bug Reports": "https://github.com/zampie/zampie_utils/issues",
+        "Source": "https://github.com/zampie/zampie_utils",
+    },
+)
+
+
+# pip install -e .  # 安装开发模式
+# pip install build twine  # 安装打包和上传工具
+# python -m build  # 打包
+# twine upload dist/*  # 上传
+# pip install zampie_utils  # 安装
