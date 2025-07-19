8affda6c50aca514e767c35ae22bdda830e23de3
zampie <zampielzp@gmail.com>
2025-07-19 15:53:57 +0800

    使用rich，优化日志

--- setup.py
+++ setup.py
@@ -9,12 +9,11 @@ if os.path.exists("README.md"):
 
 setup(
     name="zampie_utils",
-    version="0.5.1",
+    version="0.5.2",
     packages=find_packages(),
     install_requires=[
         "pandas",
         "chardet",
-        "colorlog",
         "rich"
     ],
     author="zampie",
--- zampie_utils/logger.py
+++ zampie_utils/logger.py
@@ -1,5 +1,5 @@
 import logging
-from colorlog import ColoredFormatter
+from rich.logging import RichHandler
 from .singleton import Singleton
 
 
@@ -20,30 +20,22 @@ class Logger(metaclass=Singleton):
         self.logger = logging.getLogger()
         self.logger.setLevel(log_level)
 
-        # 控制台处理器
-        self.console_handler_formatter = ColoredFormatter(
-            "%(log_color)s%(asctime)s - %(levelname)s - %(message)s",
-            datefmt=None,
-            reset=True,
-            log_colors={
-                "DEBUG": "cyan",
-                "INFO": "green",
-                "WARNING": "yellow",
-                "ERROR": "red",
-                "CRITICAL": "red",
-            },
-            secondary_log_colors={},
-            style="%",
-        )
-
-        self.console_handler = logging.StreamHandler()
-        self.console_handler.setFormatter(self.console_handler_formatter)
+        # 控制台处理器 - 使用 Rich
+        self.console_handler = RichHandler()
+        # Rich 自带格式化，无需额外设置 formatter
         self.logger.addHandler(self.console_handler)
 
         # 文件处理器
         self.file_handler = None
         if enable_file_handler:
             self.enable_file_handler(logfile)
+
+        # 直接指向方法，避免间接调用，无法定位到文件
+        self.info = self.logger.info
+        self.warning = self.logger.warning
+        self.error = self.logger.error
+        self.critical = self.logger.critical
+        self.debug = self.logger.debug
 
     def enable_file_handler(self, logfile="log.txt"):
         """
@@ -70,54 +62,14 @@ class Logger(metaclass=Singleton):
             self.logger.removeHandler(self.file_handler)
             self.file_handler = None
 
-    def log(self, level, message):
+    def set_log_level(self, log_level: str):
         """
-        description:
+        description: 设置日志级别
         param:
+            log_level: 日志级别
         return:
         """
-        self.logger.log(level, message)
-
-    def info(self, message):
-        """
-        description:
-        param:
-        return:
-        """
-        self.logger.info(message)
-
-    def warning(self, message):
-        """
-        description:
-        param:
-        return:
-        """
-        self.logger.warning(message)
-
-    def error(self, message):
-        """
-        description:
-        param:
-        return:
-        """
-        self.logger.error(message)
-
-    def critical(self, message):
-        """
-        description:
-        param:
-        return:
-        """
-        self.logger.critical(message)
-
-    def debug(self, message):
-        """
-        description:
-        param:
-        return:
-        """
-        self.logger.debug(message)
-
+        self.logger.setLevel(log_level)
 
 if __name__ == "__main__":
     # 默认不启用文件处理器
